7f19e3f99b61041e30dfbf5b9978053a0a11ce20
pani2004 <debashispani61@gmail.com>
2025-08-31 22:22:22 +0530

    Project Setup

---
+++ src/main/java/com/example/Patient_Management_System/PatientManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Patient_Management_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientManagementSystemApplication.class, args);
+	}
+
+}
